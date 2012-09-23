0794766994d320f3c584a930d6b596aa6dc23694
Zahara F Khan <Zfak102@gmail.com>
2012-09-23 07:31:28 +0100

    Initial commit

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -1,3 +1,10 @@
 class DataFile < ActiveRecord::Base
   attr_accessible :Category, :Info1, :Info2
+  has_and_belongs_to_many :users
+
+  validates :user_id, presence: true
+  validates :Info1, presence: true, length: { maximum: 140 }
+
 end
+
+
--- app/models/links_table.rb
+++ app/models/links_table.rb
@@ -1,3 +1,13 @@
 class LinksTable < ActiveRecord::Base
   attr_accessible :count, :link1, :link2
+
+  belongs_to :data_file
+
+  validates :link1, presence: true
+  validates :link2, presence: true
+
 end
+
+    def counter
+      
+    end
