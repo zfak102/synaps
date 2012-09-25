eaa534c3baecdc6c07cd6e228acd6a6aa6c5a925
Zahara F Khan <Zfak102@gmail.com>
2012-09-25 13:36:00 +0100

    dataveiw

--- app/models/deleterelationship.rb
+++
@@ -1,9 +0,0 @@
-class Relationship < ActiveRecord::Base
-  attr_accessible :datainfo_id, :userinfo_id
-
-  belongs_to :userinfo, :class_name => "User", :foreign_key => "user" 
-  belongs_to :datainfo, :class_name => "Data_file", :foreign_key => "data_file"
-
-  validates :userinfo_id, presence: true
-  validates :datainfo_id, presence: true
-end
